5295069107c4fe2c09ef13b983ebad7d4524e13e
Alan Fernandez <alan.f@msn.com>
2024-10-11 11:56:19 +0200

    updated mock data

--- src/store/mockData/notes.mockData.ts
+++ src/store/mockData/notes.mockData.ts
@@ -1,5 +1,4 @@
-import { Note } from "@store/queries/notes";
-
+import { Note } from '@store/queries/notes';
 
 const notesMockData: Note[] = [
   {
@@ -8,26 +7,30 @@ const notesMockData: Note[] = [
     content:
       'Discuss project timeline and deadlines. Need to assign tasks for the upcoming sprint.',
     folderId: 1,
+    modifiedDate: 1696896000000,
   },
   {
     id: 2,
     title: 'Shopping List',
     content: '',
-    folderId: 2,
+    folderId: 1,
+    modifiedDate: 1696809600000,
   },
   {
     id: 3,
     title: 'React Native Tips',
     content:
       'Remember to optimize images, use FlatList for performance with large data sets.',
-    folderId: 3,
+    folderId: 1,
+    modifiedDate: 1696723200000,
   },
   {
     id: 4,
     title: 'Travel Itinerary',
     content:
       'Day 1: Arrival and city tour. Day 2: Visit the museum and the local market. Day 3: Hiking trip.',
-    folderId: 1,
+    folderId: 2,
+    modifiedDate: 1696636800000,
   },
 ];
 
