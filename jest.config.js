019015216dd9acebb4ff029bed3c8ee8f58d99f8
Alan Fernandez <alan.f@msn.com>
2024-12-14 17:00:33 +0100

    chore: try to fix tests in pipeline

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,17 @@ module.exports = {
     '^.+\\.[t|j]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@screens/(.*)$': '<rootDir>/src/screens/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@navigation/(.*)$': '<rootDir>/src/navigation/$1',
+    '^@context/(.*)$': '<rootDir>/src/context/$1',
+    '^@theme/(.*)$': '<rootDir>/src/theme/$1',
+    '^@store/(.*)$': '<rootDir>/src/store/$1',
+    '^@config/(.*)$': '<rootDir>/src/config/$1',
+    '^@root/(.*)$': '<rootDir>/$1',
+  },
 }
