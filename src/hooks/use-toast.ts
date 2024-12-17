04136834ba3f48ee9294d92802178b368943e605
Alan Fernandez <alan.f@msn.com>
2024-12-17 20:55:14 +0100

    feat: use toast additional offset support

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -4,17 +4,22 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context'
 type ToastOptions = {
   isSuccess: boolean
   message: string
+  additionalOffset?: number
 }
 
 export const useToast = () => {
   const insets = useSafeAreaInsets()
 
-  const showToast = ({ isSuccess, message }: ToastOptions) => {
+  const showToast = ({
+    isSuccess,
+    message,
+    additionalOffset = 0,
+  }: ToastOptions) => {
     Toast.show({
       type: isSuccess ? 'success' : 'error',
       text1: message,
       position: 'bottom',
-      bottomOffset: insets.bottom * 3,
+      bottomOffset: insets.bottom * 3 + additionalOffset,
       visibilityTime: 1750,
     })
   }
