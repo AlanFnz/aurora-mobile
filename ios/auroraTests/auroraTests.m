787adc87db4197888dfa911297e91ff81a26c9c1
Alan Fernandez <alan.f@msn.com>
2025-01-05 13:38:17 +0100

    chore: rename project

--- ios/auroraTests/auroraTests.m
+++ ios/auroraTests/auroraTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface kiByteTests : XCTestCase
+@interface auroraTests : XCTestCase
 
 @end
 
-@implementation kiByteTests
+@implementation auroraTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
