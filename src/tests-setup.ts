4f599884b5eccc499c36dc24481efd9f9e4b5c9b
Alan Fernandez <alan.f@msn.com>
2025-02-09 17:23:25 +0100

    test: add mocks

--- src/tests-setup.ts
+++ src/tests-setup.ts
@@ -1,5 +1,43 @@
 import 'react-native-gesture-handler/jestSetup'
 
+import { Action, Middleware } from '@reduxjs/toolkit'
+
+/**
+ *
+ * TODO: use network interceptors and deprecate this
+ */
+jest.mock('@store/queries/notes.queries', () => ({
+  noteApi: {
+    reducerPath: 'noteApi',
+    reducer: (state = {}) => state,
+    middleware: (() => (next: (action: Action) => void) => (action: Action) =>
+      next(action)) as Middleware,
+  },
+  useCreateNoteMutation: () => [
+    jest.fn().mockResolvedValue({ id: 1, title: 'Test Note' }),
+  ],
+  useUpdateNoteMutation: () => [
+    jest.fn().mockResolvedValue({ id: 1, title: 'Updated Note' }),
+  ],
+  useDeleteNoteMutation: () => [jest.fn().mockResolvedValue({})],
+}))
+
+jest.mock('@store/queries/folders.queries', () => ({
+  foldersApi: {
+    reducerPath: 'foldersApi',
+    reducer: (state = {}) => state,
+    middleware: (() => (next: (action: Action) => void) => (action: Action) =>
+      next(action)) as Middleware,
+  },
+  useCreateFolderMutation: () => [
+    jest.fn().mockResolvedValue({ id: 1, folderName: 'Test Folder' }),
+  ],
+}))
+/**
+ * ////////////////////
+ * ////////////////////
+ */
+
 jest.mock('@react-native-async-storage/async-storage', () => ({
   __esModule: true,
   default: {
