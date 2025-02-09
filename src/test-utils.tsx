46895fa000565d3e21a2d3f70e6305fc9332f105
Alan Fernandez <alan.f@msn.com>
2025-02-09 17:10:50 +0100

    test: add test utils

---
+++ src/test-utils.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { Provider as ReduxProvider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+
+import { DialogProvider } from '@context/dialog'
+
+import { foldersMockData } from '@store/mocks'
+import { foldersApi } from '@store/queries/folders.queries'
+import { noteApi } from '@store/queries/notes.queries'
+import authReducer from '@store/slices/auth.slice'
+import foldersReducer from '@store/slices/folders.slice'
+
+const rootReducer = {
+  auth: authReducer,
+  folders: foldersReducer,
+  [foldersApi.reducerPath]: foldersApi.reducer,
+  [noteApi.reducerPath]: noteApi.reducer,
+}
+
+const createTestStore = (preloadedState = {}) =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(foldersApi.middleware, noteApi.middleware),
+    preloadedState,
+  })
+
+interface RenderWithProvidersOptions {
+  initialState?: Record<string, unknown>
+}
+
+export const renderWithProviders = (
+  children: React.ReactNode,
+  {
+    initialState = { folders: foldersMockData },
+  }: RenderWithProvidersOptions = {},
+) => {
+  const store = createTestStore(initialState)
+
+  return render(
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <DialogProvider>{children}</DialogProvider>
+      </NavigationContainer>
+    </ReduxProvider>,
+  )
+}
