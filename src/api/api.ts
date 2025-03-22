fb5813139533764bf7963b859755fa037681d6da
Alan Fernandez <alan.f@msn.com>
2025-03-22 18:31:14 +0100

    feat: handle session refresh in interceptors

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,16 +3,80 @@ import axios from 'axios'
 
 import { API_URL } from '@env'
 
+// TODO: refactor
+
 export const axiosInstance = axios.create({
   baseURL: `${API_URL}/api`,
   headers: { 'Content-Type': 'application/json' },
 })
 
+let isRefreshing = false
+let failedQueue: any[] = []
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach(prom => {
+    if (error) {
+      prom.reject(error)
+    } else {
+      prom.resolve(token)
+    }
+  })
+  failedQueue = []
+}
+
 axiosInstance.interceptors.request.use(async config => {
-  const token = await AsyncStorage.getItem('userToken')
+  const token = await AsyncStorage.getItem('accessToken')
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
+
+axiosInstance.interceptors.response.use(
+  res => res,
+  async error => {
+    const originalRequest = error.config
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true
+
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject })
+        })
+          .then(token => {
+            originalRequest.headers.Authorization = `Bearer ${token}`
+            return axiosInstance(originalRequest)
+          })
+          .catch(err => Promise.reject(err))
+      }
+
+      isRefreshing = true
+
+      try {
+        const refreshToken = await AsyncStorage.getItem('refreshToken')
+        const response = await axios.post(`${API_URL}/api/auth/refresh`, {
+          refreshToken,
+        })
+
+        const { accessToken } = response.data
+
+        await AsyncStorage.setItem('accessToken', accessToken)
+
+        processQueue(null, accessToken)
+
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`
+        return axiosInstance(originalRequest)
+      } catch (err) {
+        processQueue(err, null)
+        await AsyncStorage.removeItem('accessToken')
+        await AsyncStorage.removeItem('refreshToken')
+        return Promise.reject(err)
+      } finally {
+        isRefreshing = false
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
 
 // TODO: refine error handling
 export const handleApiError = (error: any) => {
