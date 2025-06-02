fea331cd2c2d7af24124acbb772ace4e07b25e57
Alan Fernandez <alan.f@msn.com>
2025-06-02 20:31:46 +0200

    feat: add update and delete queries

--- src/hooks/use-folders.ts
+++ src/hooks/use-folders.ts
@@ -1,5 +1,10 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { fetchFolders, createFolder, deleteFolder } from '@services/folder'
+import {
+  fetchFolders,
+  createFolder,
+  deleteFolder,
+  updateFolder,
+} from '@services/folder'
 
 export const useFetchFolders = () => {
   return useQuery({
@@ -24,6 +29,17 @@ export const useDeleteFolder = () => {
   const queryClient = useQueryClient()
   return useMutation({
     mutationFn: deleteFolder,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['folders'] })
+    },
+  })
+}
+
+export const useUpdateFolder = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: ({ id, folderName }: { id: number; folderName: string }) =>
+      updateFolder(id, folderName),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['folders'] })
     },
